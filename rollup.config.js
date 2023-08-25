a1d5c918552c5cacfa873ee479a746fdb4daee7f
Duy Tuan <duytuanvn1986@gmail.com>
2023-08-25 11:01:50 +0200

    final v2

--- rollup.config.js
+++ rollup.config.js
@@ -1,19 +1,29 @@
 // rollup.config.mjs
+import scss from 'rollup-plugin-scss';
 import terser from '@rollup/plugin-terser';
+import postcss from 'postcss';
+import autoprefixer from 'autoprefixer';
 export default {
   input: 'assets/js/main.js',
   output: [
     {
-      file: 'bundle.js',
+      file: 'assets/js/main-expanded.js',
       format: 'iife',
     },
     {
-      file: 'bundle.min.js',
+      file: 'assets/js/main.js',
       format: 'iife',
       name: 'version',
       plugins: [terser()],
     },
   ],
 
-  plugins: [scss()],
+  plugins: [
+    scss({
+      processor: () => postcss([autoprefixer()]),
+      sourceMap: true,
+      fileName: 'assets/css/main.css',
+      outputStyle: 'compressed',
+    }),
+  ],
 };
